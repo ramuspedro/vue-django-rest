3165ccbbbb0f47eae8929ef6c672b53c382b68c0
Pedro Ramos <pedro.ramos@terras.agr.br>
2018-05-15 17:43:40 -0300

    url auth

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 # api/urls.py
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('lead/', views.ListLead.as_view()),
     path('lead/<int:pk>/', views.DetailLead.as_view()),
+    path('lead/rest-auth/', include('rest_auth.urls')),
 ]
