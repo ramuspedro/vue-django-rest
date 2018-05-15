110ec29041134b8b527339b5ed7aa62e36c88606
Pedro Ramos <pedro.ramos@terras.agr.br>
2018-05-15 17:12:35 -0300

    adicionando rest auth

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken', # new!
+    'rest_auth', # new!
     'api',
     'leads'
 ]
