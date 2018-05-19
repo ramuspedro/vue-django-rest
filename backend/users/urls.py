d4780de3d131afee6efa14a4e91e52c5e996dd0c
Pedro Ramos <ramuspedro@gmail.com>
2018-05-19 20:07:42 -0300

    user detail

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-  path('', views.UserListView.as_view())
+  path('', views.UserListView.as_view()),
+  path('<int:pk>/', views.UserDetailView.as_view())
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from users.models import CustomUser
-from api.serializers import UserSerializer
+from users.serializers import UserSerializer
 from rest_framework import generics
 
 class UserListView(generics.ListCreateAPIView):
+  queryset = CustomUser.objects.all()
+  serializer_class = UserSerializer
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
   queryset = CustomUser.objects.all()
   serializer_class = UserSerializer
